f47d8b3c7851429775b0e7eead73c9aaee520830
abdullah eissa <abdullaheissa010@gmail.com>
2023-12-01 15:17:39 +0200

    js

--- js/master.js
+++ js/master.js
@@ -206,7 +206,7 @@ window.onscroll = function () {
   for (let i = 0; i < timelineLeft.length; i++) {
     if (
       window.scrollY >=
-      timeline.offsetTop + timelineLeft[i].offsetTop - 100
+      timeline.offsetTop + timelineLeft[i].offsetTop - 250
     ) {
       timelineLeft[i].style.opacity = 1;
     }
@@ -214,7 +214,7 @@ window.onscroll = function () {
   for (let i = 0; i < timelineRight.length; i++) {
     if (
       window.scrollY >=
-      timeline.offsetTop + timelineRight[i].offsetTop - 100
+      timeline.offsetTop + timelineRight[i].offsetTop - 250
     ) {
       timelineRight[i].style.opacity = 1;
     }
